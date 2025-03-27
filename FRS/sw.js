1dc5a0e27b40d1e6517da9676621e3de05ad1a16
Shamser Chand <39694222+shamserchand1@users.noreply.github.com>
2025-03-27 16:11:44 +0545

    Update sw.js

--- FRS/sw.js
+++ FRS/sw.js
@@ -4,41 +4,51 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/icon.png',
+  '/error.html',
+  '/face-api.min.js',
+  'https://shamserchand.com.np/FRS/models/ssd_mobilenetv1_model-weights_manifest.json',
+  'https://shamserchand.com.np/FRS/models/face_landmark_68_model-weights_manifest.json',
+  'https://shamserchand.com.np/FRS/models/face_recognition_model-weights_manifest.json',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
-  'https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap',
-  'face-api.min.js'
+  'https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        // Return cached response if found, otherwise fetch from network.
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // On failure, return the custom error page.
+        return caches.match('/error.html');
+      })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      ))
   );
 });
